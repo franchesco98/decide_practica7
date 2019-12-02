f633c8dbf3d355a66b3e84d9a620d24306eed622
Francisco Javier <frasanmat1@alum.us.es>
2019-12-02 16:05:52 +0100

    Commit configuraciones cambiadas

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://heroku-meet-travis.herokuapp.com'
+BASEURL = 'http://decide-practica7.herokuapp.com'
 
 APIS = {
     'authentication': BASEURL,
